9a773943412277a7f67e33dfc5d689e951e53851
WasabiDeLimon <74627504+WasabiDeLimon@users.noreply.github.com>
2020-11-17 13:43:19 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecuaciones_Paramétricas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ecuaciones Paramétricas";
+            Console.WriteLine("Valores en x");
+            for (int t = -1; t<=5; t++)
+            {
+                double res = t - 7;
+                Console.WriteLine("x en {0} = {1} ", t, res);
+            }
+            Console.WriteLine("\nValores en y");
+            for (int t= -1; t<=5; t++)
+            {
+                double res = (t * 2) - 7;
+                Console.WriteLine("y en {0} = {1} ", t, res);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
